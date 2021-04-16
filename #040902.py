0415c9d04fd2e7ed4ec226512a134c61a918cd4c
dohi1004 <dohi100430@gmail.com>
2021-04-16 10:51:23 +0900

    Update #040902.py

--- #040902.py
+++ #040902.py
@@ -3,22 +3,20 @@
 
 #  2)  순열 확인 : 문자열 두 개가 주어졌을 때 이 둘이 서로 순열 관계에 있는지 확인하는 메서드를 작성하라.
 
-# In[13]:
+# In[8]:
 
 
 def checkpermutation(string1, string2):
     string2 = list(string2)
-    count = 0
     if(len(string1) != len(string2)):
         print("False")
     else:
         for i in string1:
             for j in string2:
                 if(i == j):
-                    count += 1
-                    string2[string2.index(j)] = -1
+                    string2.pop(string2.index(j))
                     break
-        if(len(string1) == count):
+        if(len(string2) == 0):
             print("True")
         else:
             print("False")
