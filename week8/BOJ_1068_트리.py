e02cabdc5d7c0d3d76c1d5da8f73f67637b1dfab
dohi1004 <dohi100430@gmail.com>
2021-06-11 13:16:57 +0900

    Update BOJ_1068_트리.py

--- week8/BOJ_1068_트리.py
+++ week8/BOJ_1068_트리.py
@@ -5,25 +5,23 @@
 # 트리가 주어졌을 때, 노드 하나를 지우고 남은 트리에서 리프 노드의 개수 구하기
 # 
 # ##### 주의
-# 1) 루트 노드가 지워 졌을 때 (트리 자체가 날라가 -> 0)
-# 
-# 2) 노드 삭제 시 리프 개수 변동 구현
+# 루트 노드가 지워 졌을 때 (트리 자체가 날라가 -> 0)
 
-# In[ ]:
+# In[10]:
 
 
 def dfs(x):
     global answer
-    if not tree[x]:
+    if not tree[x]: # 더이상 자식이 없는 경우 (leaf)
         answer += 1
         return
 
-    for v in tree[x]:
+    for v in tree[x]: # 자식 있으니 자식노드 더보기
         dfs(v)
 
-N = int(input())
+N = int(input()) # 트리
 info = list(map(int, input().split()))
-tree = [[] for _ in range(N)]
+tree = [[] for _ in range(N)] # 각 트리에 자식 몇개있는지 넣을거야 인덱스로
 delete = int(input()) # 삭제할 숫자
 
 for i in range(N):
@@ -32,7 +30,7 @@ for i in range(N):
         continue
     if i == delete: # 삭제해야 하는거면 건너뛰기
         continue
-    tree[info[i]].append(i) # 그 외의 경우 tree에 값넣어
+    tree[info[i]].append(i) # 그 외의 경우 tree에 값넣어(index로! 개수가 주어져있으니까)
 
 
 answer = 0
